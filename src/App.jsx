7a6a03e002ddaf498121fd43e572b89e4e1f8a90
Brian Zhang <brian.w.zhang@gmail.com>
2024-08-30 20:11:17 -0400

    temporarily set default route to flight for testing, revert back later

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Flight />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
           <Route path="explore" element={<Explore />} />
